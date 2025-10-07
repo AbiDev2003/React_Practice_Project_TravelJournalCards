b4d896c854dcc24ee4a5c335ae11445829167c2e
AbiDev2003 <2003abinashdash@gmail.com>
2025-10-07 20:01:33 +0530

    image src path changed

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header(){
     return(
         <div className="header">
-            <img src="./images/globe.png" alt="no image" />
+            <img src="./../images/globe.png" alt="no image" />
             <h1>My Travel Journal</h1>
         </div>
     )
